e3c12ed5c961b610559b144888a347fd6a430345
Connery MacRae <macrae.connery@gmail.com>
2024-03-14 14:13:13 -0400

    entry detail added
    entry detail template created and linked via url. blog/views and blog/urls   updated. tests for entry detailview added

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 
 # Create your models here.
 class Entry(models.Model):
@@ -14,3 +14,15 @@ class Entry(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('entry_detail', kwargs={'pk': self.pk})
+
+class Comment(models.Model):
+    entry = models.ForeignKey(Entry)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    modified_at = models.DateTimeField(auto_now=True, editable=False)
+
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views import generic
+from .models import Entry
+
+
+class EntryDetail(generic.DetailView):
+    model = Entry
