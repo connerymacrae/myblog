b05ab2cb98a33e2d0d627d02af08d6c66b6eeffb
Connery MacRae <macrae.connery@gmail.com>
2024-03-19 13:08:44 -0400

    comment form test

--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-from .models import Entry, Comment
+
+from blog.forms import CommentForm
+from blog.models import Entry, Comment
 # Create your tests here.
 
 
@@ -86,10 +88,46 @@ class EntryViewTest(TestCase):
         self.assertContains(response, self.entry.comment_set.first().name)
 
     def test_comment_body_in_entry(self):
-        Comment.objects.create(entry=self.entry, name='commenter-1', body='body-1')
+        test_comment = Comment.objects.create(entry=self.entry, name='commenter-1', body='body-1')
+        test_comment.save()
         response = self.client.get(self.entry.get_absolute_url())
         self.assertContains(response, self.entry.comment_set.first().body)
 
     def test_no_comment_response(self):
         response = self.client.get(self.entry.get_absolute_url())
         self.assertContains(response, "No comments yet.")
+
+class CommentFormTest(TestCase):
+
+    def setUp(self):
+        user = get_user_model().objects.create_user('zoidberg')
+        self.entry = Entry.objects.create(author=user, title="My entry title")
+
+    def test_init(self):
+        CommentForm(entry=self.entry)
+
+    def test_init_without_entry(self):
+        with self.assertRaises(KeyError):
+            CommentForm()
+
+    def test_valid_data(self):
+        form = CommentForm({
+            'name': "Turanga Leela",
+            'email': "leela@example.com",
+            'body': "Hi there",
+        }, entry=self.entry)
+        self.assertTrue(form.is_valid())
+        comment = form.save()
+        self.assertEqual(comment.name, "Turanga Leela")
+        self.assertEqual(comment.email, "leela@example.com")
+        self.assertEqual(comment.body, "Hi there")
+        self.assertEqual(comment.entry, self.entry)
+
+    def test_blank_data(self):
+        form = CommentForm({}, entry=self.entry)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors, {
+            'name': ['required'],
+            'email': ['required'],
+            'body': ['required'],
+        })
